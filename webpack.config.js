53d3ba3a57d172091c09acb0a71806d337bdec2c
Ruslan Nekhoroshkin <nrvru@yandex.ru>
2016-06-19 16:45:07 +0300

    Добавлены настройки для production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const { resolve } = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => {
+  const addPlugin = (add, plugin) => add ? plugin : undefined;
+  const ifProduction = plugin => addPlugin(env.production, plugin);
+  const removeEmpty = plugins => plugins.filter(p => !!p);
+
   return {
     context: resolve(__dirname, 'src'),
     entry: './index.js',
@@ -16,9 +21,27 @@ module.exports = env => {
       ]
     },
     devtool: env.production ? 'source-map' : 'eval',
-    plugins: [new HtmlWebpackPlugin({
-      template: './index.html'
-    })],
+    plugins: removeEmpty([
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      ifProduction(new webpack.optimize.DedupePlugin()),
+      ifProduction(new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      })),
+      ifProduction(new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      })),
+      ifProduction(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          screw_ie8 : true,
+          warnings: false
+        }
+      }))
+    ]),
     module: {
       loaders: [
         {
