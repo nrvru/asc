99568c1c40d167b03c6eb0e71a0296e68749e26f
Ruslan Nekhoroshkin <nrvru@yandex.ru>
2016-06-25 12:28:37 +0300

    Добавлен webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,57 +3,71 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = env => {
-  const addPlugin = (add, plugin) => add ? plugin : undefined;
-  const ifProduction = plugin => addPlugin(env.production, plugin);
-  const removeEmpty = plugins => plugins.filter(p => !!p);
+  const ifProduction = (a, b) => {
+    if (env.production) {
+      return a;
+    }
+    return b;
+  };
+  const ifDevelopment = (a, b) => {
+    if (env.development) {
+      return a;
+    }
+    return b;
+  };
+  const removeEmpty = items => items.filter(i => !!i);
 
   return {
     context: resolve(__dirname, 'src'),
-    entry: './index.js',
+    entry: removeEmpty([
+      ifDevelopment('webpack-dev-server/client?http://localhost:8080/'),
+      ifDevelopment('webpack/hot/dev-server'),
+      './index.js',
+    ]),
     output: {
-      filename: 'bundle.[chunkhash].js',
-      path: resolve(__dirname, 'dist')
+      filename: ifProduction('bundle.[chunkhash].js', 'bundle.js'),
+      path: resolve(__dirname, 'dist'),
     },
     resolve: {
       modules: [
         resolve(__dirname, 'src'),
-        'node_modules'
-      ]
+        'node_modules',
+      ],
     },
     devtool: env.production ? 'source-map' : 'eval',
     plugins: removeEmpty([
       new HtmlWebpackPlugin({
-        template: './index.html'
+        template: './index.html',
       }),
       ifProduction(new webpack.optimize.DedupePlugin()),
       ifProduction(new webpack.LoaderOptionsPlugin({
         minimize: true,
-        debug: false
+        debug: false,
       })),
       ifProduction(new webpack.DefinePlugin({
         'process.env': {
-          NODE_ENV: '"production"'
-        }
+          NODE_ENV: '"production"',
+        },
       })),
       ifProduction(new webpack.optimize.UglifyJsPlugin({
         compress: {
-          screw_ie8 : true,
-          warnings: false
-        }
-      }))
+          screw_ie8: true,
+          warnings: false,
+        },
+      })),
     ]),
     module: {
       loaders: [
         {
           test: /\.js$/,
-          loaders: env.test ? 'babel' : 'babel!eslint',
-          exclude: /node_modules/
+          loaders: 'babel',
+          exclude: /node_modules/,
         },
         {
           test: /\.css$/,
-          loader: 'style!css'
-        }
-      ]
-    }
+          loader: 'style!css',
+        },
+      ],
+    },
   };
 };
